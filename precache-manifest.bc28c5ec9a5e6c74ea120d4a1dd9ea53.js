12fae4805ecae292dd011ac520b8ec8d255a3e7f
armangupta910 <120269476+armangupta910@users.noreply.github.com>
2024-05-25 01:10:00 +0530

    Updates

---
+++ precache-manifest.bc28c5ec9a5e6c74ea120d4a1dd9ea53.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2dd99fa375728acee9979f13feae29ec",
+    "url": "/Off-Campus-Opportunities/index.html"
+  },
+  {
+    "revision": "9c9b80c0a5fc6a12a117",
+    "url": "/Off-Campus-Opportunities/static/css/main.91da39c2.chunk.css"
+  },
+  {
+    "revision": "8c373453b1d2fbb75789",
+    "url": "/Off-Campus-Opportunities/static/js/2.a13e5982.chunk.js"
+  },
+  {
+    "revision": "5099652deffd979d970e",
+    "url": "/Off-Campus-Opportunities/static/js/3.d5f86460.chunk.js"
+  },
+  {
+    "revision": "9c9b80c0a5fc6a12a117",
+    "url": "/Off-Campus-Opportunities/static/js/main.c7084dd3.chunk.js"
+  },
+  {
+    "revision": "ca8346235d879da76137",
+    "url": "/Off-Campus-Opportunities/static/js/runtime~main.2870b0e2.js"
+  }
+]);
